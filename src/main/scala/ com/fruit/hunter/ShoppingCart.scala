abe3b7944af55a94fcef0f719139a170074d18c8
Hasan Altun <hasan.altun@sky.uk>
2020-12-21 08:32:49 +0000

    empty basket fix failing test

--- src/main/scala/ com/fruit/hunter/ShoppingCart.scala
+++ src/main/scala/ com/fruit/hunter/ShoppingCart.scala
@@ -1,7 +1,9 @@
-package  com.fruit.hunter
+package com.fruit.hunter
 
 trait Fruit
 
 object ShoppingCart {
-  def checkout(fruits : Seq[Fruit] = Seq()) : BigDecimal = ???
+  def checkout(fruits: Seq[Fruit] = Seq()): BigDecimal = {
+    if (fruits == null || fruits.isEmpty) 0 else ???
+  }
 }
