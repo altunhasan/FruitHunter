dc9186a1128ebdad76723b509bf702f7d73b960b
Hasan Altun <hasan.altun@sky.uk>
2020-12-21 08:39:58 +0000

    fix failing test without offer

--- src/main/scala/ com/fruit/hunter/ShoppingCart.scala
+++ src/main/scala/ com/fruit/hunter/ShoppingCart.scala
@@ -1,13 +1,20 @@
 package com.fruit.hunter
 
-sealed trait Fruit
+sealed trait Fruit {
+  def price: BigDecimal
+}
 
-case object Apple extends Fruit
+case object Apple extends Fruit {
+  override def price: BigDecimal = 0.60
+}
 
-case object Orange extends Fruit
+case object Orange extends Fruit {
+  override def price: BigDecimal = 0.25
+}
 
 object ShoppingCart {
   def checkout(fruits: Seq[Fruit] = Seq()): BigDecimal = {
-    if (fruits == null || fruits.isEmpty) 0 else ???
+    if (fruits == null || fruits.isEmpty) 0
+    else fruits.map(_.price).sum
   }
 }
